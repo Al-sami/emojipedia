b89cdf3bca02ea11b98ea2c0fbb8a7223345f35b
Al-sami <87979557+Al-sami@users.noreply.github.com>
2021-10-24 14:05:47 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+// // var newNumbers = numbers.map((x) => console.log(x * x));
+
+// ////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map((x) => x * 2);
+
+// //////Filter - Create a new array by keeping the items that return true.
+// const nEwNumbers = numbers.filter((num) => num < 10);
+
+// //Reduce - Accumulate a value by doing something to each item in an array.
+// var neUNumber = numbers.reduce(
+//   (accumulator, currentNumber) => accumulator + currentNumber
+// );
+
+// ////Find - find the first item that matches from an array.
+// const NewNumber = numbers.find((num) => num > 10);
+
+// ////FindIndex - find the index of the first item that matches.
+// const neWNumber = numbers.findIndex((num) => num > 10);
